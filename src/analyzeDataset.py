e9a122c41607290c2d4432a56be2b546ce8c237d
DiogoCarvalho13 <j.d.t.c882@gmail.com>
2023-05-18 22:47:09 +0100

    changes to svm for normalization of distance

--- src/analyzeDataset.py
+++ src/analyzeDataset.py
@@ -10,21 +10,44 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.svm import SVC
+
+def holdout_estimation(X,y,models,ts=0.3,seed=0):
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X,y,test_size=ts,random_state=seed)
+    for m in models:
+        m.fit(X_train,y_train)
+        y_pred = m.predict(X_test)
+        print(f'{m} Accuracy:{accuracy_score(y_test,y_pred)}')
+        plot_decisionBound(m,X_train,y_train)
+
+
+def plot_decisionBound(model,X,y):
+    disp = DecisionBoundaryDisplay.from_estimator(
+        model, X, response_method="predict",
+        alpha=0.5)
+    disp.ax_.scatter(X['X1'], X['X2'], c=y, edgecolor="k")
+    plt.show()
 
 
 def plot_ds2D(X,y):
     sns.scatterplot(x=X[0],y=X[1],hue=y,palette="deep")
     plt.show()
 
-dataset = openml.datasets.get_dataset('credit-g')
+dataset = openml.datasets.get_dataset('diabetes')
 
 X, y, categorical_indicator, attribute_names = dataset.get_data(
     dataset_format="array", target=dataset.default_target_attribute
 )
 
-plt.scatter(X[:, 0], X[:, 1], c=y)
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title(f'Dataset {dataset.name}')
-plt.colorbar(label='Target')
-plt.show()
+linear_svc = SVC(kernel="linear")
+
+
+print(X.shape)
+
+#plt.scatter(X[:, 0], X[:, 1], c=y)
+#plt.xlabel('Feature 1')
+#plt.ylabel('Feature 2')
+#plt.title(f'Dataset {dataset.name}')
+#plt.colorbar(label='Target')
+#plt.show()
